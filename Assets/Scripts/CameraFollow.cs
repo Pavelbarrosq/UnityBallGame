5c2e8d8698c53d59ea438571abb3dd408e94363d
Pavel Barros Quintanilla <pavelbarrosquintanilla@Pavels-MacBook-Pro.local>
2019-09-13 12:02:03 +0200

    init game,player and movement

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform playerTransform;
+    public float cameraDistance = 40.0f;
+    public float yPos = 2.0f;
+
+
+    private void Awake()
+    {
+        GetComponent<UnityEngine.Camera>().orthographicSize = ((Screen.height / 2) / cameraDistance);
+    }
+
+    private void FixedUpdate()
+    {
+        CameraPosition(yPos);
+    }
+
+    private void CameraPosition(float position)
+    {
+        transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y + position, transform.position.z);
+    }
+
+    
+}
---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public GameObject player;
+
+    public Vector2 startPosition;
+    public Vector2 direction;
+    public bool directionChoosen;
+
+
+    private void Start()
+    {
+       
+    }
+
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    startPosition = touch.position;
+
+                    break;
+
+                case TouchPhase.Moved:
+                    direction = touch.position - startPosition;
+
+                    break;
+
+                case TouchPhase.Ended:
+                    directionChoosen = true;
+                    break;
+            }
+        }
+
+        if (directionChoosen)
+        {
+            
+            player.transform.position = direction;
+
+            directionChoosen = false;
+        }
+    }
+}
