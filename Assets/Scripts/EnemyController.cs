11b62e90a15f950d9eb749d0e34bc29141181332
Pavel Barros Quintanilla <pavelbarrosquintanilla@Pavels-MacBook-Pro.local>
2019-09-25 13:49:07 +0200

    Enemy Created, Player pushed up after colliding,

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private Material matWhite;
+    private Material matDefault;
+    public float waitToKillTime = 0.3f;
+
+    public Rigidbody2D playerRB;
+    public float pushUp = 0.5f;
+    
+
+    SpriteRenderer sr;
+
+
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+
+
+        matWhite = Resources.Load("WhiteFlash",typeof(Material)) as Material;
+        matDefault = sr.material;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.CompareTag("Player"))
+        {
+            playerRB.drag = 10000;
+            sr.material = matWhite;
+            Debug.Log("Target hit!");
+            
+
+            StartCoroutine(KillEnemy(waitToKillTime));
+
+            PushPlayerUp();
+        }
+    }
+
+    private void PushPlayerUp()
+    {
+        playerRB.drag = 0;
+
+        playerRB.AddForce(Vector2.up * pushUp);
+        
+    }
+
+    private IEnumerator KillEnemy(float waitTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,5 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private void Update()
-    {
-        
-    }
+    
 }
