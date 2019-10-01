0f59968de1f344355a54b45f0c9ed9e251ace0b3
Pavel Barros Quintanilla <pavelbarrosquintanilla@Pavels-MacBook-Pro.local>
2019-10-01 10:43:28 +0200

    Updated code

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -10,11 +10,15 @@ public class MovementController : MonoBehaviour
     private float distance;
 
     public float forceMulti = 0.5f;
+    public float setDrag = 1;
     public Vector3 startPos;
     public Vector2 direction;
     public bool directionChoosen = false;
     private Vector3 touchDragDistance;
     private CameraFollow cameraFollow;
+
+    public float delay = 0.5f;
+    private bool orNah = false;
 
 
     private void Awake()
@@ -30,33 +34,24 @@ public class MovementController : MonoBehaviour
         //timeCompelete = false;
     }
 
-    private void Update()
-    {
-        
-
-    }
-
-    
-
     private void FixedUpdate()
     {
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
             Vector3 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
-
             startPos.z = 0f;
             startPos = playerPos.position;
 
             switch (touch.phase)
             {
                 case TouchPhase.Began:
-
                     //cameraFollow.HandleZoom(true);
                     startPos = touchPos;
+                    //StartCoroutine(SetRBComponent(delay, true));
                     rb.gravityScale = 0;
                     rb.mass = 0.1f;
-                    rb.drag = 1;
+                    rb.drag = setDrag;
                     break;
 
                 case TouchPhase.Moved:
@@ -65,23 +60,40 @@ public class MovementController : MonoBehaviour
                     break;
 
                 case TouchPhase.Ended:
-
                     //cameraFollow.HandleZoom(false);
-
                     distance = touchPos.magnitude;
-
-                    rb.drag = 0;
-                    rb.mass = 1f;
-                    rb.gravityScale = 1;
-
                     direction = touchPos - startPos;
 
                     rb.AddForce(direction * forceMulti);
+                    rb.gravityScale = 1;
+                    rb.mass = 1;
+                    rb.drag = 0;
+                    //StartCoroutine(SetRBComponent(delay, false));
+                    
 
                     Debug.Log(distance);
                     directionChoosen = true;
+
                     break;
             }
         }
     }
+
+    //private IEnumerator SetRBComponent(float delay, bool orNah)
+    //{
+    //    if (!orNah)
+    //    {
+    //        yield return new WaitForSeconds(delay);
+    //        rb.gravityScale = 1;
+    //        rb.mass = 1;
+    //        rb.drag = 0;
+    //    } else
+    //    {
+    //        yield return new WaitForSeconds(delay);
+    //        rb.gravityScale = 0;
+    //        rb.mass = 0.1f;
+    //        rb.drag = 1;
+    //    }
+    //}
+
 }
