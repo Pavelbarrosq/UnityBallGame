ac6ad35ae0c99ce64453ed18929c72487bb5551d
Pavel Barros Quintanilla <pavelbarrosquintanilla@Pavels-MacBook-Pro.local>
2019-10-02 16:20:40 +0200

    Astro and asteriod changed to ball

---
+++ Assets/_Game/Scripts/MovementController.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public Transform playerPos;
+    public GameObject player;
+    private Rigidbody2D rb;
+    public LineRenderer lr;
+
+    
+    Vector3 lineStartPos;
+    Vector3 lineEndPos;
+
+    private float distance;
+
+    public float forceMulti = 0.5f;
+    public float setDrag = 1;
+    private Vector3 startPos;
+    private Vector2 direction;
+    private bool directionChoosen = false;
+    private Vector3 touchDragDistance;
+    private CameraFollow cameraFollow;
+
+    public float delay = 0.5f;
+
+
+
+    private void Awake()
+    {
+        rb = player.GetComponent<Rigidbody2D>();
+        playerPos = player.GetComponent<Transform>();
+        
+        
+    }
+
+    private void Start()
+    {
+        cameraFollow = Camera.main.GetComponent<CameraFollow>();
+        //timeCompelete = false;
+    }
+
+    private void FixedUpdate()
+    {
+        lineStartPos = playerPos.position;
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector3 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+            startPos.z = 0f;
+            startPos = playerPos.position;
+            lr.positionCount = 2;
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    
+                    //cameraFollow.HandleZoom(true);
+                    startPos = touchPos;
+                    //StartCoroutine(SetRBComponent(delay, true));
+                    rb.gravityScale = 0;
+                    rb.mass = 0.1f;
+
+
+                    rb.drag = setDrag;
+                    lr.enabled = true;
+                    lineStartPos = playerPos.position;
+                    lr.SetPosition(0, lineStartPos);
+                    lr.useWorldSpace = true;
+
+                    break;
+
+                case TouchPhase.Moved:
+                    lineEndPos = touchPos;
+                    lr.SetPosition(0, lineStartPos);
+                    lr.SetPosition(1, lineEndPos);
+
+                    break;
+
+                case TouchPhase.Ended:
+                    lr.enabled = false;
+                    //cameraFollow.HandleZoom(false);
+                    distance = touchPos.magnitude;
+                    direction = touchPos - startPos;
+
+                    rb.AddForce(direction * forceMulti);
+                    
+                    rb.gravityScale = 1;
+                    rb.mass = 1;
+                    rb.drag = 0;
+                    //StartCoroutine(SetRBComponent(delay, false));
+
+
+                    Debug.Log("SLUT!");
+                    directionChoosen = true;
+
+                    break;
+            }
+        }
+    }
+
+    
+
+    //private IEnumerator SetRBComponent(float delay, bool orNah)
+    //{
+    //    if (!orNah)
+    //    {
+    //        yield return new WaitForSeconds(delay);
+    //        rb.gravityScale = 1;
+    //        rb.mass = 1;
+    //        rb.drag = 0;
+    //    } else
+    //    {
+    //        yield return new WaitForSeconds(delay);
+    //        rb.gravityScale = 0;
+    //        rb.mass = 0.1f;
+    //        rb.drag = 1;
+    //    }
+    //}
+
+}
---
+++ Assets/_Game/Scripts/ObjectiveBallController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectiveBallController : MonoBehaviour
+{
+    private Material matWhite;
+    private Material matDefault;
+    public float pushUp = 0.5f;
+    public float waitToKillTime = 0.3f;
+
+    private SpriteRenderer sr;
+    private CircleCollider2D cc;
+
+    public Rigidbody2D playerRB;
+    private UnityEngine.Object explosionRef;
+
+
+    private void Start()
+    {
+        cc = playerRB.GetComponentInParent<CircleCollider2D>();
+        sr = GetComponent<SpriteRenderer>();
+        matWhite = Resources.Load("WhiteFlash", typeof(Material)) as Material;
+        matDefault = sr.material;
+        explosionRef = Resources.Load("Explosion");
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            
+            playerRB.bodyType = RigidbodyType2D.Static;
+            //playerRB.gravityScale = 0;
+            //playerRB.mass = 0.1f;
+            //playerRB.drag = 10000;
+            sr.material = matWhite;
+            Debug.Log("Target hit!");
+
+
+            StartCoroutine(KillSelf(waitToKillTime));
+
+            
+            //PushPlayerUp();
+        }
+    }
+
+    private void PushPlayerUp()
+    {
+        //playerRB.drag = 0;
+
+
+        playerRB.AddForce(Vector2.up * pushUp);
+
+
+
+        //playerRB.mass = 1;
+
+        //playerRB.gravityScale = 1;
+    }
+
+    private IEnumerator KillSelf(float waitTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            Destroy(gameObject);
+            AddParticleExplosion();
+            playerRB.bodyType = RigidbodyType2D.Dynamic;
+        }
+    }
+
+    private void AddParticleExplosion()
+    {
+        GameObject explosion = (GameObject)Instantiate(explosionRef);
+
+        explosion.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+
+    }
+}
