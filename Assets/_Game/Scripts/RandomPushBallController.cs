4607256bee48292d8ba1dbaf47c8ab636d1cc658
Pavel Barros Quintanilla <pavelbarrosquintanilla@Pavels-MacBook-Pro.local>
2019-10-22 11:34:26 +0200

    added random slingshot ball

---
+++ Assets/_Game/Scripts/RandomPushBallController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomPushBallController : MonoBehaviour
+{
+    private Rigidbody2D playerRB;
+
+    private Vector2 randomDegree;
+
+    public int addForce = 100;
+
+    private float secondsToWait = 0.05f;
+
+    private Object explosionRef;
+
+    private Image content;
+    private ScoreCounter scoreCounter;
+    public float halfLife = 0.5f;
+
+
+
+    private void Awake()
+    {
+        playerRB = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+
+        randomDegree = Random.insideUnitCircle;
+
+        explosionRef = Resources.Load("SlingShotExplosion");
+
+        content = GameObject.FindGameObjectWithTag("Content").GetComponent<Image>();
+        scoreCounter = FindObjectOfType<ScoreCounter>();
+
+        
+    }
+
+    private void Update()
+    {
+        randomDegree = Random.insideUnitCircle;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            scoreCounter.AddScore();
+            scoreCounter.AddScore();
+
+            AddHealth(halfLife);
+
+            playerRB.bodyType = RigidbodyType2D.Static;
+
+            Debug.Log("Random degree is" + randomDegree);
+
+            StartCoroutine(PushPLayer());
+
+        }
+    }
+
+    IEnumerator PushPLayer()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(secondsToWait);
+            Destroy(this.gameObject);
+            Explosion();
+
+            FindObjectOfType<AudioManager>().Play("SlingShot");
+
+            playerRB.bodyType = RigidbodyType2D.Dynamic;
+            playerRB.AddForce(randomDegree * addForce);
+        }
+    }
+
+    private void Explosion()
+    {
+        GameObject explosion = Instantiate(explosionRef) as GameObject;
+        explosion.transform.position = this.transform.position;
+    }
+
+    private void AddHealth(float health)
+    {
+        content.fillAmount += health;
+    }
+}
---
+++ Assets/_Game/Scripts/RandomPushBallSpawner.cs
@@ -0,0 +1,185 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class RandomPushBallSpawner : MonoBehaviour
+{
+    public GameObject randomPushBallPrefab;
+
+
+    public int RandomPushPerField = 3;
+
+    [SerializeField] private float fieldSize = 15f;
+
+    private Vector2Int lastField;
+
+
+    List<GameObject> RandomBalls = new List<GameObject>();
+
+
+    private void Awake()
+    {
+        SpawnOnAwake(randomPushBallPrefab, RandomPushPerField, RandomBalls);
+
+        //Vector2Int currentField = GetCurrentField();
+
+        //PopulateField(currentField);
+
+        //PopulateNeighbouringFields(currentField);
+
+        //PopulateOuterNeighbouringFields(currentField);
+
+    }
+
+    private void SpawnOnAwake(GameObject ballPrefab, int ballsPerField, List<GameObject> ballList)
+    {
+        Vector2Int currentField = GetCurrentField();
+
+        PopulateField(currentField, ballPrefab, ballsPerField, ballList);
+
+        PopulateNeighbouringFields(currentField, ballPrefab, ballsPerField, ballList);
+
+        PopulateOuterNeighbouringFields(currentField, ballPrefab, ballsPerField, ballList);
+    }
+
+
+    private void Update()
+    {
+        PopulateOnUpdate(randomPushBallPrefab, RandomPushPerField, RandomBalls);
+
+    }
+
+    private void PopulateOnUpdate(GameObject ballPrefab, int ballsPerField, List<GameObject> ballList)
+    {
+        Vector2Int currentField = GetCurrentField();
+
+        if (Time.frameCount % 2 == 0)
+        {
+
+            if (lastField != currentField) // om vi har bytt fällt
+            {
+
+                for (int i = ballList.Count - 1; i >= 0; i--)
+                {
+
+                    if (ballList[i] == null)
+                    {
+                        ballList.RemoveAt(i);
+                    }
+                    else if (GetField(ballList[i].transform.position) != currentField
+                        && !NeighbouringFields(currentField).Contains<Vector2Int>(GetField(ballList[i].transform.position))) // om boll int är i current eller neighbour
+                    {
+                        Destroy(ballList[i]);
+                        ballList.RemoveAt(i);
+                    }
+                }
+
+                PopulateOuterNeighbouringFields(currentField, ballPrefab, ballsPerField, ballList);
+                lastField = currentField;
+            }
+        }
+
+    }
+
+
+    private void PopulateNeighbouringFields(Vector2Int centerField, GameObject ballPrefab, int ballsPerField, List<GameObject> ballList)
+    {
+        // Vector2Int currentField = GetCurrentField();
+
+        foreach (Vector2Int field in NeighbouringFields(centerField))
+        {
+            PopulateField(field, ballPrefab, ballsPerField, ballList);
+        }
+
+        lastField = centerField;
+
+    }
+
+    private void PopulateOuterNeighbouringFields(Vector2Int centerField, GameObject ballPrefab, int ballsPerField, List<GameObject> ballList)
+    {
+
+        foreach (Vector2Int field in OuterNeighbouringFields(centerField))
+        {
+            PopulateField(field, ballPrefab, ballsPerField, ballList);
+        }
+
+    }
+
+
+    private void PopulateField(Vector2Int field, GameObject ballPrefab, int ballsPerField, List<GameObject> ballList)
+    {
+
+
+        for (int i = 0; i < ballsPerField; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-fieldSize, fieldSize) + (field.x * 2 * fieldSize), Random.Range(-fieldSize, fieldSize) + (field.y * 2 * fieldSize), 0);
+
+            GameObject obj = Instantiate(ballPrefab, pos, Quaternion.identity);
+            ballList.Add(obj);
+
+
+        }
+    }
+
+    private Vector2Int[] NeighbouringFields(Vector2Int field)
+    {
+        Vector2Int[] neighbours = new Vector2Int[8];
+
+        neighbours[0] = new Vector2Int(field.x - 1, field.y + 1);
+        neighbours[1] = new Vector2Int(field.x, field.y + 1);
+        neighbours[2] = new Vector2Int(field.x + 1, field.y + 1);
+        neighbours[3] = new Vector2Int(field.x - 1, field.y);
+        neighbours[4] = new Vector2Int(field.x + 1, field.y);
+        neighbours[5] = new Vector2Int(field.x - 1, field.y - 1);
+        neighbours[6] = new Vector2Int(field.x, field.y - 1);
+        neighbours[7] = new Vector2Int(field.x + 1, field.y - 1);
+
+        return neighbours;
+    }
+
+
+    private Vector2Int[] OuterNeighbouringFields(Vector2Int field)
+    {
+
+        Vector2Int[] neighbours = new Vector2Int[16];
+
+
+        neighbours[0] = new Vector2Int(field.x - 2, field.y + 2);
+        neighbours[1] = new Vector2Int(field.x - 1, field.y + 2);
+        neighbours[2] = new Vector2Int(field.x, field.y + 2);
+        neighbours[3] = new Vector2Int(field.x + 1, field.y + 2);
+        neighbours[4] = new Vector2Int(field.x + 2, field.y + 2);
+
+        neighbours[5] = new Vector2Int(field.x - 2, field.y + 1);
+        neighbours[6] = new Vector2Int(field.x + 2, field.y + 1);
+
+        neighbours[7] = new Vector2Int(field.x - 2, field.y);
+        neighbours[8] = new Vector2Int(field.x + 2, field.y);
+
+        neighbours[9] = new Vector2Int(field.x - 2, field.y - 1);
+        neighbours[10] = new Vector2Int(field.x + 2, field.y - 1);
+
+        neighbours[11] = new Vector2Int(field.x - 2, field.y - 2);
+        neighbours[12] = new Vector2Int(field.x - 1, field.y - 2);
+        neighbours[13] = new Vector2Int(field.x, field.y - 2);
+        neighbours[14] = new Vector2Int(field.x + 1, field.y - 2);
+        neighbours[15] = new Vector2Int(field.x + 2, field.y - 2);
+
+        return neighbours;
+    }
+
+
+    private Vector2Int GetCurrentField()
+    {
+        return GetField(transform.position);
+    }
+
+
+    private Vector2Int GetField(Vector2 pos)
+    {
+
+        return new Vector2Int(Mathf.RoundToInt(pos.x / (fieldSize * 2)), Mathf.RoundToInt(pos.y / (fieldSize * 2)));
+
+    }
+}
